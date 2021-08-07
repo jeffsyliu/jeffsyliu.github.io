3f304d7ae9293948e79c0af66ebe05ac99d922cc
Sheng Yang Liu <shengyangliu@Shengs-MacBook-Pro.local>
2021-08-07 23:43:31 +0800

    feat: finish skills section

---
+++ src/panels/SkillItem.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Rating from "@material-ui/lab/Rating";
+
+const SkillItem = ({ skill, level }) => {
+  return (
+    <Grid
+      item
+      container
+      justifyContent="space-between"
+      alignItems="center"
+      xs={12}
+      sm={6}
+    >
+      <Grid item xs={6}>
+        <Typography variant="subtitle1" display="inline">
+          {skill}
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Rating
+          defaultValue={level}
+          precision={0.5}
+          readOnly
+          className="rating"
+        />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SkillItem;
